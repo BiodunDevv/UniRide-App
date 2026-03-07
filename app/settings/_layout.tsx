8abec4978191ab43bc8ea275af6c57b2c0a810d1
Muhammed Abiodun <muhammedabiodun42@gmail.com>
2026-03-08 00:17:10 +0100

    refactor: remove unused biometric and notification-detail screens from settings layout

--- app/settings/_layout.tsx
+++ app/settings/_layout.tsx
@@ -16,11 +16,9 @@ export default function SettingsLayout() {
         <Stack.Screen name="edit-profile" />
         <Stack.Screen name="edit-driver-profile" />
         <Stack.Screen name="change-password" />
-        <Stack.Screen name="biometric" />
         <Stack.Screen name="security" />
         <Stack.Screen name="devices" />
         <Stack.Screen name="notification-settings" />
-        <Stack.Screen name="notification-detail" />
         <Stack.Screen name="bank-picker" />
         <Stack.Screen name="vehicle" />
       </Stack>
